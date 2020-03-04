a14de1b6d9d6df4bb00693a756046d3975076890
asaran97 <asaran234@gmail.com>
2020-03-05 02:00:30 +0530

    :sparkles: added animation and images

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Url from '../shorten';
 
-import img from '../../assets/ques.png';
+import defaultImg from '../../assets/ques.png';
+import errorImg from '../../assets/error.png';
+import loadingImg from '../../assets/loading.png';
+import successImg from '../../assets/success.png';
 import './style.scss';
 
 const Main = () => {
+  const [img, setImg] = useState(defaultImg)
+
+  const imgToRender = (type) => {
+    console.log(type)
+    switch (type) {
+      case 'loading':
+        setImg(loadingImg)
+        break;
+      case 'error':
+        setImg(errorImg)
+        break;
+      case 'success':
+        setImg(successImg)
+        break;
+      default:
+        setImg(defaultImg)
+        break;
+    }
+  }
+
   return (
     <div className='main'>
       <div className="left-container">
         <img src={img} alt="img"/>
       </div>
       <div className="right-container">
-        <Url />
+        <div className="title">
+          <h1>Paste the URL to be shortened</h1>
+        </div>
+        <Url imgRenderer={imgToRender}/>
       </div>
     </div>
   )
--- src/components/shorten/index.js
+++ src/components/shorten/index.js
@@ -4,7 +4,7 @@ import axois from 'axios';
 
 import './style.scss';
 
-const Main = () => {
+const Main = ({imgRenderer}) => {
   const [value, setValue] = useState('');
   const [valid, setValid] = useState(null);
   const [errorMsg, setErrorMsg] = useState(false);
@@ -12,25 +12,37 @@ const Main = () => {
   const [data, setData] = useState(null);
 
   const onSubmit = () => {
-    if(!valid) {
+    const isvalidUrl = isUrl(value);
+    setValid(isvalidUrl);
+
+    if(!isvalidUrl) {
       setErrorMsg('please enter a valid url')
+      imgRenderer('error')
     } else {
+      imgRenderer('loading')
+      setErrorMsg(null);
       setIsFetching(true);
       const url=`${process.env.REACT_APP_BASE_API_URL}/api/url/shorten`;
       axois.post(url, {
         longUrl: value
       }, {
         "Content-Type":"application/json"
-      }).then(({data}) => setData(data))
-      .catch(err=> console.log(err))
+      })
+      .then(({data}) => {
+        imgRenderer('success')
+        setData(data)
+        setIsFetching(false);
+      })
+      .catch(err=> {
+        console.log(err)
+        setIsFetching(false);
+      })
     }
   }
 
   const handleChange = (e) => {
     const typedUrl = e.target.value
-    const isvalidUrl = isUrl(typedUrl);
     setValue(typedUrl);
-    setValid(isvalidUrl);
   }
 
   const copyText = () => {
@@ -45,11 +57,35 @@ const Main = () => {
       <div className="container">
 
         <div className="url-container">
-          <input type="text" className="input" value={value} placeholder="Enter a url.." onChange={handleChange}/>
-          <button className='submit-btn' onClick={onSubmit}>Shorten URL</button>
+          <input type="text" className={`input ${errorMsg ? 'error': ''} ${data && !isFetching ? 'success' : ''}`} value={value} placeholder="Enter a url.." onChange={handleChange}/>
+
+          <div className={`error-container ${errorMsg ? 'show' : ''}`}>
+            {errorMsg}
+          </div>
+
+          <div className={`submit-btn ${data && !isFetching ? 'hide' : ''}`} onClick={onSubmit}>
+            <span className='text'>Shorten URL</span>
+          </div>
         </div>
-        <br/>
-        {data && (<div className="result-container">
+
+        <div className={`result-container ${data && !isFetching ? 'show' : ''}`}>
+          <div className="long-url">
+            <div className="heading">
+              Original Link
+            </div>
+            <div className="value">
+              {value.length < 100 ? value : value.slice(0, 100)+ '...'}
+            </div>
+          </div>
+          <div className="result">
+            {data && data.shortUrl}
+            <button className='copy-btn' onClick={copyText}>copy</button>
+          </div>
+        </div>
+
+
+        {/* {data && (
+        <div className="result-container">
           <div className="long-url">
             <div className="heading">
               Original Link
@@ -62,7 +98,8 @@ const Main = () => {
             {data && data.shortUrl}
             <button className='copy-btn' onClick={copyText}>copy</button>
           </div>
-        </div>)}
+        </div>)
+        } */}
       </div>
 
     </div>
